56a5a3cd7a4e2f4a0791a262b746ddee000d8f96
Ben <ben.talese@gmail.com>
2023-04-13 22:58:30 +1000

    Add pipes and pipe infrastructure

--- pipeline.py
+++ pipeline.py
@@ -1,58 +1,75 @@
 from abc import ABC, abstractmethod
-from typing import Generic
-from .generics import TInputPort, TOutputPort
+from typing import Callable, Generic
+
+from generics import TInputPort, TOutputPort
 
 
-class PipePriority:
-    __initialized = False
-
-    def __init__(self, **kwargs):
-        if not self.__initialized:
-            for key, value in self.__GetDefaultValues().items():
-                setattr(self, key, value)
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-        self.__initialized = True
-
-    def __GetDefaultValues(self):
-        return {
-            f'{IAuthenticationVerifier.__name__}': 1,
-            f'{IEntityExistenceChecker.__name__}': 2,
-            f'{IAuthorisationEnforcer.__name__}': 3,
-            f'{IBusinessRuleValidator.__name__}': 4,
-            f'{IInputPortValidator.__name__}': 5,
-            f'{IInteractor.__name__}': 6,
-        }
-
-    def __setattr__(self, key, value):
-        if self.__initialized and key not in self.__dict__:
-            raise TypeError("Cannot add new values to an initialized CustomEnum")
-        if key is not '_CustomEnum__initialized' and any(getattr(self, k) == value for k in self.__dict__):
+class PipePriorityMeta(type):
+    
+    def __setattr__(cls, key, value):
+        if any(getattr(cls, _Key) == value for _Key in cls.__dict__ if _Key != key):
             raise ValueError(f"Cannot assign pipe priority '{value}' to '{key}'. Priority '{value}' is in use by another pipe.")
+            
         super().__setattr__(key, value)
+
+
+class PipePriority(metaclass=PipePriorityMeta):
+    pass
 
 
 class IPipe(Generic[TInputPort, TOutputPort], ABC):
 
-    def __init__(self):
-        self.m_CanInvokeNextPipe = True
-
     @property
-    def CanInvokeNextPipe(self) -> bool:
-        return self.m_CanInvokeNextPipe
+    @abstractmethod
+    def priority(self) -> PipePriority:
+        pass
+
+    @abstractmethod
+    def execute(self, input_port: TInputPort, output_port: TOutputPort) -> Callable | None:
+        pass
+
+
+class IAuthenticationVerifier(IPipe, ABC):
     
     @property
-    @abstractmethod
-    def Priority(self) -> int: # TODO: this was "-> PipePriority", but this should be built in a way the user can easily configure it
-        pass                    # Also, probably don't want IPipe to be aware of how it is ordered
-
-    @abstractmethod
-    def Execute(self, inputPort: TInputPort, outputPort: TOutputPort) -> bool:
-        pass
+    def priority(self) -> PipePriority:
+        return PipePriority.IAuthenticationVerifier
 
 
 class IAuthorisationEnforcer(IPipe, Generic[TInputPort, TOutputPort], ABC):
     
     @property
-    def Priority(self) -> PipePriority:
-        return PipePriority.AuthorisationEnforcer
+    def priority(self) -> PipePriority:
+        return PipePriority.IAuthorisationEnforcer
+
+
+class IBusinessRuleValidator(IPipe, Generic[TInputPort, TOutputPort], ABC):
+    
+    @property
+    def priority(self) -> PipePriority:
+        return PipePriority.IBusinessRuleValidator
+
+
+class IEntityExistenceChecker(IPipe, Generic[TInputPort, TOutputPort], ABC):
+    
+    @property
+    def priority(self) -> PipePriority:
+        return PipePriority.IEntityExistenceChecker
+
+
+class IInputPortValidator(IPipe, Generic[TInputPort, TOutputPort], ABC):
+    
+    @property
+    def priority(self) -> PipePriority:
+        return PipePriority.IInputPortValidator
+
+
+class IInputPort(ABC):
+    pass
+
+
+class IInteractor(IPipe, Generic[TInputPort, TOutputPort], ABC):
+
+    @property
+    def priority(self) -> PipePriority:
+        return PipePriority.IInteractor
