4a6da8fe340186019cb9bd091d79728be3419d24
Ben <ben.talese@gmail.com>
2023-04-27 22:17:52 +1000

    Remove redundant output port, add docs

--- outputs.py
+++ outputs.py
@@ -3,29 +3,41 @@ from typing import Generic
 
 from generics import TAuthorisationFailure, TValidationFailure
 
+
 class IAuthenticationOutputPort(ABC):
-    
+    '''
+    An output port for when authentication is required by the use case.
+    '''
+
     @abstractmethod
     async def present_unauthenticated_async() -> None:
+        '''
+        Presents an authentication failure.
+        '''
         pass
 
 
 class IAuthorisationOutputPort(Generic[TAuthorisationFailure], ABC):
-    
+    '''
+    An output port for when authorisation is required by the use case.
+    '''
+
     @abstractmethod
     async def present_unauthorised_async(self, authorisation_failure: TAuthorisationFailure) -> None:
-        pass
-    
-
-class IBusinessRuleValidationOutputPort(Generic[TValidationFailure], ABC):
-    
-    @abstractmethod
-    async def present_business_rule_validation_failure_async(self, validation_failure: TValidationFailure) -> None:
+        '''
+        Presents an authorisation failure.
+        '''
         pass
 
 
 class IValidationOutputPort(Generic[TValidationFailure], ABC):
+    '''
+    An output port for when validation is required.
+    '''
     
     @abstractmethod
     async def present_validation_failure_async(self, validation_failure: TValidationFailure) -> None:
+        '''
+        Presents a validation failure.
+        '''
         pass
