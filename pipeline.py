5356305235c76cb66d8c3e5b487d2e4cc302475c
Ben <ben.talese@gmail.com>
2023-04-28 17:26:19 +1000

    Add docs to pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
-from typing import Callable, Coroutine, Generic
+from typing import Coroutine, Generic
 
 from generics import TInputPort, TOutputPort
 
 
 class PipePriorityMeta(type):
-    
+    '''The `PipePriorityMeta` class prevents assigning the same priority value to multiple pipes.'''
     def __setattr__(cls, key, value):
         if any(getattr(cls, _Key) == value for _Key in cls.__dict__ if _Key != key):
             raise ValueError(f"Cannot assign pipe priority '{value}' to '{key}'. Priority '{value}' is in use by another pipe.")
@@ -14,62 +14,81 @@ class PipePriorityMeta(type):
 
 
 class PipePriority(metaclass=PipePriorityMeta):
+    '''Defines the priorities of pipes.'''
     pass
 
 
 class IPipe(Generic[TInputPort, TOutputPort], ABC):
-
+    '''Marks a class as a pipe. A pipe is a class that must have an execution method and a priority.'''
     @property
     @abstractmethod
     def priority(self) -> PipePriority:
+        '''Defines the priority of the pipe within the pipeline.'''
         pass
 
     @abstractmethod
     async def execute_async(self, input_port: TInputPort, output_port: TOutputPort) -> Coroutine | None:
+        '''
+        Summary
+        -------
+        Defines the behaviour of the pipe when executed. Must return either a coroutine
+        function (an output port method), or no result.
+        
+        Parameters
+        ----------
+        `input_port` The input of the use case to be processed\n
+        `output_port` The interface containing output methods to return as a result of the pipe's execution
+
+        Returns
+        -------
+        `Coroutine` (a method of the output port) if the pipe has a result to return, otherwise `None`.
+        
+        '''
         pass
 
 
 class IAuthenticationVerifier(IPipe, ABC):
-    
+    '''Marks a class as an authentication verifier pipe.'''
     @property
     def priority(self) -> PipePriority:
         return PipePriority.IAuthenticationVerifier
 
 
 class IAuthorisationEnforcer(IPipe, Generic[TInputPort, TOutputPort], ABC):
-    
+    '''Marks a class as an authorisation enforcer pipe.'''
     @property
     def priority(self) -> PipePriority:
         return PipePriority.IAuthorisationEnforcer
 
 
 class IBusinessRuleValidator(IPipe, Generic[TInputPort, TOutputPort], ABC):
-    
+    '''Marks a class as a business rule validator pipe.'''
     @property
     def priority(self) -> PipePriority:
         return PipePriority.IBusinessRuleValidator
 
 
 class IEntityExistenceChecker(IPipe, Generic[TInputPort, TOutputPort], ABC):
-    
+    '''Marks a class as an entity existence checker pipe.'''
     @property
     def priority(self) -> PipePriority:
         return PipePriority.IEntityExistenceChecker
 
 
 class IInputPortValidator(IPipe, Generic[TInputPort, TOutputPort], ABC):
-    
+    '''Marks a class as an input port validator pipe.'''
     @property
     def priority(self) -> PipePriority:
         return PipePriority.IInputPortValidator
 
 
 class IInputPort(ABC):
+    '''Marks a class as an input port (not an implementation of IPipe).'''
     pass
 
 
 class IInteractor(IPipe, Generic[TInputPort, TOutputPort], ABC):
-
+    '''Marks a class as an interactor pipe.'''
     @property
     def priority(self) -> PipePriority:
         return PipePriority.IInteractor
