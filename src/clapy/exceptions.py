fe51efc6040107e322bd62aca24301f80f9c4eaf
Ben <ben.talese@gmail.com>
2023-06-24 00:29:41 +1000

    Add PipeConfigurationError exception

--- src/clapy/exceptions.py
+++ src/clapy/exceptions.py
@@ -1,3 +1,7 @@
 class DuplicateServiceError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
+
+class PipeConfigurationError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
