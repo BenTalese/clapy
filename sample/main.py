e4ad21d14e72a0b8c5f86d0c0fc7a9f65a1346d1
Ben <ben.talese@gmail.com>
2023-06-24 00:00:37 +1000

    Add test pipe to DI container and fix enum usage

--- sample/main.py
+++ sample/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+
 sys.path.append(os.getcwd())
 
 import asyncio
@@ -9,6 +10,7 @@ from dependency_injector import providers
 
 from sample.interface_adaptors.conversation_controller import \
     ConversationController
+from sample.pipeline.test_pipe import TestPipe
 from sample.interface_adaptors.greet_presenter import GreetPresenter
 from sample.pipeline.pipeline_configuration import PipelineConfiguration
 from sample.use_cases.greet.greet_input_port import GreetInputPort
@@ -26,10 +28,11 @@ async def main():
     _ServiceProvider.construct_usecase_invoker(_UsecaseScanLocations)
 
     _ServiceProvider.register_service(providers.Factory, ConversationController)
+    _ServiceProvider.register_service(providers.Factory, TestPipe)
 
     _Controller = _ServiceProvider.get_service(ConversationController)
 
-    await _Controller.greet_async(GreetInputPort("Ben"), GreetPresenter(), PipelineConfiguration.DefaultConfiguration)
+    await _Controller.greet_async(GreetInputPort("Ben"), GreetPresenter(), PipelineConfiguration.DefaultConfiguration.value)
 
 if __name__ == "__main__":
     asyncio.run(main())
