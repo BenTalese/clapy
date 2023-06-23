f908a099fe98c7db9eea321bfa35c3acccd16479
Ben <ben.talese@gmail.com>
2023-06-23 23:56:48 +1000

    Tidy generics

--- src/clapy/generics.py
+++ src/clapy/generics.py
@@ -2,13 +2,7 @@ from typing import TypeVar
 
 
 TAuthorisationFailure = TypeVar("TAuthorisationFailure")
-TInputPort = TypeVar("TInputPort")  # TODO: This one is questionable too...
-TInteractor = TypeVar("TInteractor")    # TODO: Is this even used anywhere anymore?
+TInputPort = TypeVar("TInputPort")
 TOutputPort = TypeVar("TOutputPort")
 TServiceType = TypeVar("TServiceType")
 TValidationFailure = TypeVar("TValidationFailure")
-
-# TAuthorisationFailure = TypeVar("TAuthorisationFailure", bound=IAuthorisationResult)
-# TODO: Investigate if IAuthorisationResult is worth doing
-# TValidationFailure = TypeVar("TValidationFailure", bound=IValidationResult)
-# TODO: Investigate if IValidationResult is worth doing
